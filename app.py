995f853a132a3d6d8962423a90a43f5d252c86b2
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-06-20 05:14:47 +0530

    test commmit

--- app.py
+++ app.py
@@ -2,10 +2,23 @@
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 
+import tweepy
+import os
+
+import time
+
+from click._compat import raw_input
+
+
+
 app = Flask(__name__)
 app.secret_key='ayush'
 num=0
-
+lvl=0
+ver=0
+counter=0
+login=0
+auth=tweepy.OAuthHandler('t5qZhGyVwTkNArktAPM64nSvl','lk2ViVadYV6JbyeY7KLRfcDSxV9aGdn9ez9pTTO8cylnO7Z16J')
 
 @app.before_request
 def make_session_permanent():
@@ -20,14 +33,48 @@ def hello():
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
-    global num
+    global lvl
+    global counter
+    global ver
+    global login
+    resp = MessagingResponse()
+
     session['phone_no']=request.form.get('From')
     msg = request.form.get('Body')
-    if(msg=='*'):
+    if lvl==0:
+        if counter==0:
+            resp.message("Hi there. Login to Twitter here. {} And send the code".format(auth.get_authorization_url()))
+            counter=1
+        else:
+            token = auth.request_token['oauth_token']
+            verifier = msg
+            ver=1
+        lvl=1
 
-        num=10
+    if ver==1:
+        auth.request_token = {'oauth_token': token,
+                              'oauth_token_secret': verifier}
+        try:
+            auth.get_access_token(verifier)
+            login=1
+        except tweepy.TweepError:
+            print('Error! Failed to get access token.')
+            login=0
+
+            key = auth.access_token
+            secret = auth.access_token_secret
+
+            auth.set_access_token(key, secret)
+
+
+
+    if login==1:
+        pi = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        user = api.me()
+
+        resp.message("Hi, {}".format(user.screen_name))
+
     # Create reply
-    resp = MessagingResponse()
 
     # session.permanent = True
     if(msg=='69'):
