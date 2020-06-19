5886c76eb088547c7e0e3b1a7c1525371dabb38a
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-06-19 15:17:07 +0530

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request
+from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -12,11 +12,17 @@ def hello():
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
+    session['phone_no'] = 'admin'
     msg = request.form.get('Body')
-    phone_no=request.form.get('From')
+    if(msg=='*'):
+        session['phone_no']=request.form.get('From')
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {} using {}".format(msg,phone_no))
+    if(msg=='69'):
+        resp.message("Session is {}".format(session['phone_no']))
+    else:
+
+        resp.message("You said: {} ".format(msg))
 
     return str(resp)
 
