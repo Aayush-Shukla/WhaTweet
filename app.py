cb1e3cb2b181ad21ead5bb627b2deb2424eb0398
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-06-20 08:10:38 +0530

    test

--- app.py
+++ app.py
@@ -34,70 +34,70 @@ def hello():
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
-    global init
-    if init==0:
-        session['phone_no']=request.form.get('From')
-        init=1
-
-    token=''
-    global lvl
-    global counter
-    global ver
-    global login
+    # global init
+    # if init==0:
+    #     session['phone_no']=request.form.get('From')
+    #     init=1
+    #
+    # token=''
+    # global lvl
+    # global counter
+    # global ver
+    # global login
     resp = MessagingResponse()
-    resp.message("{}{}{}{}{}{}".format(num,ver,counter,login,init,session['phone_no']))
+    # resp.message("{}{}{}{}{}{}".format(num,ver,counter,login,init,session['phone_no']))
     msg = request.form.get('Body')
-
-    if lvl==0:
-        if counter==0:
-            resp.message("Hi there. Login to Twitter here. {} And send the code".format(auth.get_authorization_url()))
-            counter=1
-            resp.message("{}".format(counter))
-        else:
-            token = auth.request_token['oauth_token']
-            verifier = msg
-            resp.message("Verifier code :".format(verifier))
-
-            ver=1
-            lvl=1
-
-    if ver==1:
-        auth.request_token = {'oauth_token': token,
-                              'oauth_token_secret': verifier}
-        try:
-            auth.get_access_token(verifier)
-            login=1
-        except tweepy.TweepError:
-            resp.message('Error! Failed to get access token.')
-            # lvl=0
-            # ver=0
-            # counter=0
-            # login=0
-
-            resp.message("login:".format(login))
-
-            key = auth.access_token
-            secret = auth.access_token_secret
-
-            auth.set_access_token(key, secret)
-
-
-
-    if login==1:
-        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-        user = api.me()
-        resp.message("yo")
+    #
+    # if lvl==0:
+    #     if counter==0:
+    #         resp.message("Hi there. Login to Twitter here. {} And send the code".format(auth.get_authorization_url()))
+    #         counter=1
+    #         resp.message("{}".format(counter))
+    #     else:
+    #         token = auth.request_token['oauth_token']
+    #         verifier = msg
+    #         resp.message("Verifier code :".format(verifier))
+    #
+    #         ver=1
+    #         lvl=1
+    #
+    # if ver==1:
+    #     auth.request_token = {'oauth_token': token,
+    #                           'oauth_token_secret': verifier}
+    #     try:
+    #         auth.get_access_token(verifier)
+    #         login=1
+    #     except tweepy.TweepError:
+    #         resp.message('Error! Failed to get access token.')
+    #         # lvl=0
+    #         # ver=0
+    #         # counter=0
+    #         # login=0
+    #
+    #         resp.message("login:".format(login))
+    #
+    #         key = auth.access_token
+    #         secret = auth.access_token_secret
+    #
+    #         auth.set_access_token(key, secret)
+    #
+    #
+    #
+    # if login==1:
+    #     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    #     user = api.me()
+    #     resp.message("yo")
         # start= resp.message("Hi, {} ({})\n What would you like to do?\n1. Make Tweet\n".format(user.name,user.screen_name))
         # start.media("https://twitter.com/{}/photo".format(user.screen_name))
 
     # Create reply
 
-    # session.permanent = True
-    # if(msg=='69'):
-    #     resp.message("Session is {}".format(num))
-    # else:
-    #
-    #     resp.message("You said: {} ".format(msg))
+    session.permanent = True
+    if(msg=='69'):
+        resp.message("Session is {}".format(num))
+    else:
+
+        resp.message("You said: {} ".format(msg))
     return str(resp)
 
 if __name__ == "__main__":
