0edb523c2198e3dbea2591ca9c21301ed9e48db7
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-07-02 00:40:48 +0530

    test

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import file as file
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+
+engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
+)
 
 
 import tweepy
