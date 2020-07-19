b558a7b6df57e1ed5cbd8f667bb8ad4669ffbb39
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-07-19 07:12:17 +0530

    edit utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-CONSUMER_KEY = 't5qZhGyVwTkNArktAPM64nSvl'
-CONSUMER_SECRET = 'lk2ViVadYV6JbyeY7KLRfcDSxV9aGdn9ez9pTTO8cylnO7Z16J'
-DATABASE= 'postgres://xdqieazjvunfne:95c006314ec65cb73f0c7895ef905c64b6606932a7d13cbcab22cb7bdf7a0c0d@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d3hdstc8500olp'
+CONSUMER_KEY =
+CONSUMER_SECRET =
+DATABASE=
