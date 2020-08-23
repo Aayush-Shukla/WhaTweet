92dd11ab498fc978d94c8fd1e3a2fd1adce03d01
Ayush Shukla <ayush.inazumaxi@gmail.com>
2020-08-23 19:43:50 +0530

    minor change

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-DATABASE = os.environ.get('DATABASE')
+DATABASE = os.environ.get('DATABASE_URL')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 ACCOUNT_SID = os.environ.get('account_sid')
